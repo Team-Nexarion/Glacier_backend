e9789e82da5e5f7c01fa5cad1f05291ebefe9806
Subhajit Roy <161867596+Subhajitroy03@users.noreply.github.com>
2026-01-03 00:27:20 +0530

    joy shri ram

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ const adminRouter=require("./admin.route");
 const officialRouter=require("./official.route");
 const express=require("express");
 const app=express();
+app.use("/",(req,res)=>{
+    res.send("Welcome to the Glacier Project API");
+});
 app.use("/admin",adminRouter);
 app.use("/official",officialRouter);
 module.exports=app;
